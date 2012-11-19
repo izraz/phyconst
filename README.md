559ff9a5c058262143527f4be0204842e53598d4
Bojan Durickovic <bojan@bojand.org>
2012-11-19 15:56:00 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Physical Constants
+
+This is a simple python file with physical constants defined as phython
+variables.
+
+Variable names are descriptive in CamelCase and include physical units, e.g.
+	BoltzmannConstant_JpK
+for the Botzmann constant in units J/K ("p" is for "per").
+
+## Usage
+	from phyconst import BoltzmannConstant_JpK as k
---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Some physical constants"""
+
+
+BoltzmannConstant_JpK = 1.3806503e-23 # Boltzmann constant [J/K]
+BoltzmannConstant_TorrLpK = 1.0355758239450354e-22 # Boltzmann constant [Torr L/K]
+
+ProtonCharge_C = 1.60217646e-19 # proton charge [C]
+ElectronCharge_C = - ProtonCharge_C
+
+
+Troom_K = 293.15 # room temperature [K]
+
+# First ionization potentials [V]
+IonizationPotentials_V = {'N': 14.53, # nitrogen
+                          'O': 13.61 # oxygen
+                          }
