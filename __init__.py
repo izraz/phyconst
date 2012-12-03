36f126584f512b9e0f003d295dd1c4fff9486d0d
Bojan Durickovic <bojan@bojand.org>
2012-12-03 13:34:30 -0500

    Shorten variable names for canonical SI units

--- __init__.py
+++ __init__.py
@@ -1,16 +1,26 @@
-"""Some physical constants"""
+"""Some physical constants
+
+For non-SI units, units are appended to the variable name
+"""
 
 
-BoltzmannConstant_JpK = 1.3806503e-23 # Boltzmann constant [J/K]
+BoltzmannConstant = 1.3806503e-23 # Boltzmann constant [J/K]
 BoltzmannConstant_TorrLpK = 1.0355758239450354e-22 # Boltzmann constant [Torr L/K]
 
-ProtonCharge_C = 1.60217646e-19 # proton charge [C]
-ElectronCharge_C = - ProtonCharge_C
+ProtonCharge = 1.60217646e-19 # proton charge [C]
+ElectronCharge = - ProtonCharge
 
 
-Troom_K = 293.15 # room temperature [K]
+Troom = 293.15 # room temperature [K]
 
 # First ionization potentials [V]
-IonizationPotentials_V = {'N': 14.53, # nitrogen
+IonizationPotentials = {'N': 14.53, # nitrogen
                           'O': 13.61 # oxygen
                           }
+
+# Backward compatibility
+BoltzmannConstant_JpK = BoltzmannConstant # backward compatibility only
+IonizationPotentials_V = IonizationPotentials
+Troom_K = Troom
+ProtonCharge_C = ProtonCharge
+ElectronCharge_C = ElectronCharge
